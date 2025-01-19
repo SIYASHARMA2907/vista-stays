1635c26238827ca5833a88103d95106fde8191a2
Ayush Negi <a3yushnegi@gmail.com>
2025-01-19 19:25:17 +0530

    root node redirected to /listings

--- app.js
+++ app.js
@@ -71,9 +71,9 @@ const sessionOptions = {
 
 
 
-// app.get("/", (req, res) => {
-//   res.send("hii i am root");
-// });
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
 
 app.use(session(sessionOptions));
 app.use(flash());
